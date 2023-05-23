408119ed5d69a4dba1d20c8f039dfc044b73be09
GuillaumeLFC <guigui.commerce@outlook.com>
2023-05-23 23:03:23 +0200

    ça m'a pété les couilles

--- server.js
+++ server.js
@@ -6,18 +6,13 @@ const app = express();
 
 const uploads = multer({dest : '/photos'})
 
-// Set up a route to handle incoming requests
-app.get('/', (req, res) => {
-  const upload_test = path.join(__dirname, 'upload_test.html');
-  res.sendFile(upload_test);
-});
-
-app.post('/uploads',upload.single('photo'),async (req, res) => {
+app.get
+app.post('/',uploads.array('photos'),async (req, res) => {
   res.send('Heu ça a marché ?')
 });
 
 // Start the server
 const port = 3000;
 app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+  console.log(`Le serveur tourne sur le port ${port}`);
 });
