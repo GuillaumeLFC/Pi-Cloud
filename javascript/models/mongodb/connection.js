eb98615a4205da5b0561f7d16fe16701891e6bc3
Guillaume LFC <guigui.commerce@outlook.com>
2023-06-18 00:17:09 +0200

    18/06/23

---
+++ javascript/models/mongodb/connection.js
@@ -0,0 +1,42 @@
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.connectMongo = void 0;
+const { MongoClient, ServerApiVersion } = require("mongodb");
+// Replace the placeholder with your Atlas connection string
+const uri = "mongodb://mongo";
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+function run() {
+    return __awaiter(this, void 0, void 0, function* () {
+        try {
+            // Connect the client to the server (optional starting in v4.7)
+            yield client.connect();
+            // Send a ping to confirm a successful connection
+            yield client.db("admin").command({ ping: 1 });
+            console.log("Connexion à mongoDB réussie !");
+        }
+        finally {
+            // Ensures that the client will close when you finish/error
+            yield client.close();
+        }
+    });
+}
+function connectMongo() {
+    run().catch(console.dir);
+}
+exports.connectMongo = connectMongo;
